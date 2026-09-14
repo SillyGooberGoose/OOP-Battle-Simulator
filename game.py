0048d5ed24c9f9750d21d374ac50953946189cea
SillyGooberGoose <sutton.fowler007@gmail.com>
2026-09-14 11:50:03 -0500

    Build Hero class
    Created an entire hero class, added battle system, and added funny commentary

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from goblin import Goblin
-
+from hero import Hero
+from time import sleep
 
 ARENA_NAME = "The Golden Sphere"
 
@@ -11,14 +12,32 @@ def main():
     print("The gates are opening...")
 
     goblin = Goblin("George")
-
-    print(f"{goblin.name} enters the arena with {goblin.health} health.")
-
     goblinTwo = Goblin("Harold")
-    
+    goblinDamage = goblin.attack()
+    goblinTwoDamage = goblinTwo.attack()
+    print(f"{goblin.name} enters the arena with {goblin.health} health.")
     print(f"{goblinTwo.name} enters the arena with {goblinTwo.health} health.")
-    print("But no hero has answered the call... yet.")
 
+    print("But no hero has answered the call... yet.")
+    sleep(2)
+
+    hero = Hero("Mr. Bohon")
+    heroDamage = hero.attack()
+    heroPhrase = hero.battle_cry()
+
+    print(f"{hero.name} enters the arena with {hero.health} health!")
+    sleep(1)
+    print(heroPhrase)
+    print(f"{hero.name} ignites the battle with a sudden attack!")
+
+    sleep(0.5)
+    print(f"{hero.name} attackes {goblin.name}!")
+    sleep(1)
+    goblin.take_damage(heroDamage)
+
+    sleep(0.75)
+    print(f"{goblin.name} gets mad and attacks {hero.name}!")
+    hero.take_damage(goblinDamage)
 
 if __name__ == "__main__":
     main()
--- hero.py
+++ hero.py
@@ -1,4 +1,33 @@
+from random import randint
 class Hero:
     """The hero blueprint will be implemented later in the project."""
 
-    pass
+    def __init__(self, name):
+        # Create the Hero's attributes here.
+        self.name = name
+        self.health = randint(100,150)
+        self.attack_power = randint(10,25)
+
+    def attack(self):
+        # Return a random value from 1 through this Hero's attack power.
+        return randint(1, self.attack_power)
+
+    def take_damage(self, damage):
+        # Subtract damage, but do not allow health to fall below 0.
+        self.health = max(1, self.health - damage)
+        print(f"{self.name} takes {damage} damage. Health: {self.health}")
+
+    def is_alive(self):
+        # Return a Boolean based on this Hero's health.
+        return self.health > 0
+    def battle_cry(self):
+        # Returns a randomly generated battle cry.
+        choosingPhrase = randint(1,4)
+        if choosingPhrase == 1:
+            print("I'll beat you with calculus!")
+        elif choosingPhrase == 2:
+            print("a + b = your demise!")
+        elif choosingPhrase == 3:
+            print("I can learn math while beating you up!")
+        else:
+            print("I can't think of anything funny!")
